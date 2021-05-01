16d860180de73f1f769890b53c1070b85d0b8309
Vinicius Pereira <agavi2014@hotmail.com>
2021-04-30 23:35:52 -0300

    [NEW] First COmmit

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,5 @@
+export function SignInButton() {
+    return (
+        <h1>SignInButton</h1>
+    )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import styles from '../styles/home.module.scss';
+import Head from 'next/head';
+import { Header } from '../components/Header';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>ig.news</title>
+      </Head>
+    </>
+  )
+}
