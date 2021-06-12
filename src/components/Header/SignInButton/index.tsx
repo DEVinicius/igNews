679d54e7574356bf3d2274a7abda1ad3b6d8ecdf
Vinicius Pereira de Oliveira <agavi2014@hotmail.com>
2021-06-11 23:07:03 -0300

    [NEW] Authentication

--- src/components/Header/SignInButton/index.tsx
+++ src/components/Header/SignInButton/index.tsx
@@ -1,26 +1,29 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 import styles from './styles.module.scss';
+import { signIn, useSession, signOut } from 'next-auth/client'
 
 export function SignInButton() {
-    const isUserLoggedIn = true;
+    const [session] = useSession();
     
-    return isUserLoggedIn ? (
+    return session ? (
         <button 
             className={styles.signInButton}
             type="button"
         >
             <FaGithub color="04d361"/>
-            Vinicius Pereira
+            {session.user.name}
             <FiX 
                 color="737380"
                 className={styles.closeIcon}
+                onClick={() => signOut()}
             />
         </button>
     ) : (
         <button 
             className={styles.signInButton}
             type="button"
+            onClick={() => signIn("github")}
         >
             <FaGithub color="eba417"/>
             Sign In With GitHub
